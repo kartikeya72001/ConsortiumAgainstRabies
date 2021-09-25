19b233e273438fbbe0abac68dfc0da37eb995714
Kartikeya72001 <kartikeya72001@gmail.com>
2021-09-25 21:25:33 +0530

    Worldrabiesday

---
+++ Activities/generateImages.js
@@ -0,0 +1,27 @@
+// imagePath = ["./WorldRabiesDay/WRD0000.jpeg",
+// "./WorldRabiesDay/WRD0001.jpeg",
+// "./WorldRabiesDay/WRD0002.jpeg",
+// "./WorldRabiesDay/WRD0003.jpeg",
+// "./WorldRabiesDay/WRD0004.jpeg",
+// "./WorldRabiesDay/WRD0005.jpeg",
+// "./WorldRabiesDay/WRD0006.jpeg",
+// "./WorldRabiesDay/WRD0007.jpeg",
+// "./WorldRabiesDay/WRD0008.jpeg",
+// "./WorldRabiesDay/WRD0009.jpeg",
+// "./WorldRabiesDay/WRD0010.jpeg",
+// "./WorldRabiesDay/WRD0011.jpeg",
+// "./WorldRabiesDay/WRD0012.jpeg",
+// "./WorldRabiesDay/WRD0013.jpeg",
+// "./WorldRabiesDay/WRD0014.jpeg",
+// "./WorldRabiesDay/WRD0015.jpeg",
+// "./WorldRabiesDay/WRD0016.jpeg",
+// "./WorldRabiesDay/WRD0017.jpeg",]
+// for(var i=0;i<imagePath.length;i++){
+//     var obj = `<div class="col-sm-6 col-md-4">
+//                     <a class="lightbox" href="${imagePath[i]}">
+//                         <img src="${imagePath[i]}" alt="Images">
+//                     </a>
+//                 </div>`;
+//     document.getElementById('imgSpace').innerHTML += obj;
+//     console.log(obj);
+// }
